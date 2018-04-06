f12ddb0687e171ab83884fde3abc19a68600f58a
lukaszgryglicki <lukaszgryglicki@o2.pl>
2018-04-06 07:33:05 +0200

    Analysis from 2015 including 4/2018
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- BigQuery/query.sql
+++ BigQuery/query.sql
@@ -13,7 +13,7 @@ from
   `bigquery-public-data.hacker_news.full`
 where
   REGEXP_CONTAINS(title, r'^Ask HN: Who is hiring\?\s+\(\w+\s+\d+\)$')
-  and timestamp >= TIMESTAMP("2016-01-01 00:00:00")
+  and timestamp >= TIMESTAMP("2015-01-01 00:00:00")
 union all select
   id,
   parent,
@@ -34,7 +34,7 @@ where
       `bigquery-public-data.hacker_news.full`
      where
       REGEXP_CONTAINS(title, r'^Ask HN: Who is hiring\?\s+\(\w+\s+\d+\)$')
-      and timestamp >= TIMESTAMP("2016-01-01 00:00:00")
+      and timestamp >= TIMESTAMP("2015-01-01 00:00:00")
   )
 order by
   time
